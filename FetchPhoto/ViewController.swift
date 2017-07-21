7579829cec2959374ff9eb9fca5d7ddc7672e7b4
王育翎 <yuling@wangyulingde-MacBook-Pro.local>
2017-07-21 17:38:18 +0800

    finish

--- FetchPhoto/ViewController.swift
+++ FetchPhoto/ViewController.swift
@@ -8,41 +8,79 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIScrollViewDelegate {
 
     @IBOutlet weak var addPhotoButton: UIButton!
     @IBOutlet weak var buttonBackgroundView: UIView!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoScrollView: UIScrollView!
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.toolbar.isTranslucent = true
+        
+ 
+        //button outlet
+        addPhotoButton.backgroundColor = UIColor(red: 43.0/255.0, green: 43.0/255.0, blue: 43.0/255.0, alpha:1)
+        addPhotoButton.layer.cornerRadius = 2
+        addPhotoButton.layer.shadowColor = UIColor.black.cgColor
+        addPhotoButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        addPhotoButton.setTitle("Pick an Image", for: .normal)
+        addPhotoButton.titleLabel?.font = UIFont(name: ".SFUIText-Heavy", size: 20)
+        addPhotoButton.setTitleColor(UIColor.white, for: .normal)
+        addPhotoButton.addTarget(self, action: #selector(ViewController.addPhotoButton(_:)), for: UIControlEvents.touchUpInside)
+
+        
+        //button background
+        buttonBackgroundView.backgroundColor = UIColor(red: 249.0/255.0, green: 223.0/255.0, blue: 23.0/255.0, alpha:1)
+        
+        
+        //photoImage
+//        photoImageView.image = UIImage(named: "icon_photo")
+        photoImageView.layer.backgroundColor = UIColor.black.cgColor
+        photoImageView.tintColor = UIColor.white
+        photoImageView.contentMode = .center
+//        photoImageView.contentMode = .scaleAspectFit
+
+        
+        print("00000000")
+        
+        //scroll view
+        photoScrollView.layer.backgroundColor = UIColor.black.cgColor
+        photoScrollView.minimumZoomScale = 0.1
+        photoScrollView.maximumZoomScale = 5.0
+        photoScrollView.zoomScale = 1.0  //很重要...
+        
+        photoScrollView.delegate = self
+        photoScrollView.addSubview(photoImageView)
+
+        
+    }
+
     
-//    func callGetPhoto() {
-//        
-//        let photoPicker: UIImagePickerController = UIImagePickerController()
-//        
-//        photoPicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
-//        photoPicker.allowsEditing = true // 可對照片作編輯
-//        photoPicker.delegate = self
-//        self.present(photoPicker, animated: true, completion: nil)
-//        
-//
-//    }
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
     
+        return self.photoImageView
+    }
+    
+
     func imagePickerController(_ picker: UIImagePickerController,  didFinishPickingMediaWithInfo info: [String: Any]) {
         
         picker.dismiss(animated: true, completion: nil) // 關掉
         
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             photoImageView.image = image
+            photoImageView.contentMode = .scaleAspectFit
+            
+            
         } else{
             print("Something went wrong")
         }
         
-//        self.photoImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
-//        photoPicker.image = [info objectForKey:UIImagePickerControllerOriginalImage]
         
-        print("",info[UIImagePickerControllerOriginalImage])
-    
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -54,41 +92,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let picker: UIImagePickerController = UIImagePickerController()
         
         picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
-        picker.allowsEditing = true // 可對照片作編輯
+//        picker.allowsEditing = true // 可對照片作編輯
         picker.delegate = self
         self.present(picker, animated: true, completion: nil)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-       
-        //button outlet
-        addPhotoButton.backgroundColor = UIColor(red: 43.0/255.0, green: 43.0/255.0, blue: 43.0/255.0, alpha:1)
-        addPhotoButton.layer.cornerRadius = 2
-        addPhotoButton.layer.shadowColor = UIColor.black.cgColor
-        addPhotoButton.layer.shadowOffset = CGSize(width: 0, height: 2)
-        
-        addPhotoButton.addTarget(self, action: #selector(ViewController.addPhotoButton(_:)), for: UIControlEvents.touchUpInside)
-//        addPhotoButton.addTarget(self, action:)
-        
-        
-        
-        //button background
-        buttonBackgroundView.backgroundColor = UIColor(red: 249.0/255.0, green: 223.0/255.0, blue: 23.0/255.0, alpha:1)
-        
-        
-        //photoImage
-        photoImageView.frame = CGRect(x: self.view.frame.size.width / 4 / 2, y: 64, width: self.view.frame.size.width / 4 * 3, height: self.view.frame.size.height / 4 * 2)
-        photoImageView.image = UIImage(named: "icon_photo")
-        photoImageView.layer.backgroundColor = UIColor.black.cgColor
-        photoImageView.tintColor = UIColor.white
-        
-//        photoImageView.contentMode = .scaleToFit
         
         
     }
-
-    
 
 }
 
