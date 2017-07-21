5ba5b582a1a0d7ffb77de5a857572610cf111038
王育翎 <yuling@wangyulingde-MacBook-Pro.local>
2017-07-21 14:13:37 +0800

    cannot show image

--- FetchPhoto/AppDelegate.swift
+++ FetchPhoto/AppDelegate.swift
@@ -12,10 +12,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var mainVC: ViewController?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        mainVC = ViewController()
+//        window?.rootViewController = mainVC!
+//        window!.makeKeyAndVisible()
+        
         return true
     }
 
--- FetchPhoto/ViewController.swift
+++ FetchPhoto/ViewController.swift
@@ -8,12 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var addPhotoButton: UIButton!
     @IBOutlet weak var buttonBackgroundView: UIView!
+    @IBOutlet weak var photoImageView: UIImageView!
     
-    @IBAction func addPhotoButton(_ sender: Any) {
+    
+    
+    func callGetPhoto() {
+        
+        let photoPicker: UIImagePickerController = UIImagePickerController()
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            
+            photoPicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            photoPicker.allowsEditing = true
+            photoPicker.delegate = self
+            self.present(photoPicker, animated: true, completion: nil)
+            
+        }
+    
+    }
+    
+    private func photoPickerController(_ photoPicker: UIImagePickerController,  didFinishPickingMediaWithInfo info: [String: Any]) {
+    
+        photoPicker.dismiss(animated: true, completion: nil)
+        self.photoImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+    
+    func imagePickerControllerDidCancel(_ photoPicker: UIImagePickerController) {
+        photoPicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func addPhotoButton(_ sender: UIButton) {
+        
+        self.callGetPhoto()
     }
     
     override func viewDidLoad() {
@@ -25,8 +55,25 @@ class ViewController: UIViewController {
         addPhotoButton.layer.shadowColor = UIColor.black.cgColor
         addPhotoButton.layer.shadowOffset = CGSize(width: 0, height: 2)
         
+        addPhotoButton.addTarget(self, action: #selector(ViewController.addPhotoButton(_:)), for: UIControlEvents.touchUpInside)
+//        addPhotoButton.addTarget(self, action:)
+        
+        
+        
         //button background
         buttonBackgroundView.backgroundColor = UIColor(red: 249.0/255.0, green: 223.0/255.0, blue: 23.0/255.0, alpha:1)
+        
+        
+        //photoImage
+        photoImageView.frame = CGRect(x: self.view.frame.size.width / 4 / 2, y: 64, width: self.view.frame.size.width / 4 * 3, height: self.view.frame.size.height / 4 * 2)
+        photoImageView.image = UIImage(named: "icon_photo")
+        photoImageView.layer.backgroundColor = UIColor.black.cgColor
+        photoImageView.tintColor = UIColor.white
+//        photoImageView.contentMode = .scaleToFill
+        
+        
+        self.view.addSubview(self.photoImageView)
+//        self.view.addSubview(addPhotoButton)
         
     }
 
